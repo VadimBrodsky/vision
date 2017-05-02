38e849dc0591a59181eb4a40332506b02d402eea
Vadim Brodsky <vadim.brodsky@vidyard.com>
2017-05-02 16:41:24 -0400

    Lift player state to app

--- src/components/app.js
+++ src/components/app.js
@@ -9,12 +9,33 @@ const settings = {
   source: 'sample.mp4',
 };
 
-const App = () => (
-  <div className={styles.app}>
-    <h2>Hello, World</h2>
-    <Player source={settings.source} id={settings.id} />
-    <Controls playerId={settings.id} />
-  </div>
-);
+class App extends React.Component {
+  handleMetadata = e => {
+    this.setState({
+      duration: e.target.duration
+    });
+  };
+
+  handleTimeUpdate = e => {
+    this.setState({
+      position: e.target.currentTime
+    });
+  };
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <h2>Hello, World</h2>
+        <Player
+          source={settings.source}
+          id={settings.id}
+          onMetadata={this.handleMetadata}
+          onTime={this.handleTimeUpdate}
+        />
+        <Controls playerId={settings.id} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/player.js
+++ src/components/player.js
@@ -2,21 +2,16 @@ import React from 'react';
 import Media from './media';
 
 class Player extends React.Component {
-  handleMetadata = e => {
-    this.setState({
-      duration: e.target.duration
-    });
-  };
 
-  handleTimeUpdate = e => {
-    this.setState({
-      position: e.target.currentTime
-    });
-  };
 
   render() {
     return (
-      <video width="480" id={this.props.id} onLoadedMetadata={this.handleMetadata} onTimeUpdate={this.handleTimeUpdate}>
+      <video
+        width="480"
+        id={this.props.id}
+        onLoadedMetadata={this.props.onMetadata}
+        onTimeUpdate={this.props.onTime}
+      >
         <Media source={this.props.source} />
       </video>
     );
