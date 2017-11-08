9b07445bcd3886a04da4b8180c3ff6063247550f
Vadim Brodsky <vadim.brodsky@gmail.com>
2017-11-08 06:40:38 -0500

    Export each functio separatly

--- src/helpers/media_api.js
+++ src/helpers/media_api.js
@@ -1,25 +1,18 @@
-const play = (media) => {
+export const play = (media) => {
   console.log('EVENT: Play');
   media.play();
 };
 
-const pause = (media) => {
+export const pause = (media) => {
   console.log('EVENT: Pause');
   media.pause();
 };
 
-const currentTime = (media, time) => {
+export const currentTime = (media, time) => {
   if (time !== undefined) {
     media.currentTime = time;
   }
   return media.currentTime;
 };
 
-const duration = media => media.duration;
-
-export {
-  play,
-  pause,
-  currentTime,
-  duration,
-};
+export const duration = media => media.duration;
