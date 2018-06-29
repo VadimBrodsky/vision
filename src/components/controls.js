551456ce4a9af3e9ffd961af8cf7593374aba676
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-06-28 23:51:04 -0400

    Dispatch actions based on the play button

--- src/components/controls.js
+++ src/components/controls.js
@@ -3,31 +3,38 @@ import PlayButton from './play_button';
 import ProgressBar from './progress_bar';
 import TimeDisplay from './time-display';
 
-const Controls = ({ duration, playing, position, mediaApi, liftState }) => {
+import { connect } from 'react-redux';
+import { play, pause } from '../actions';
+
+const Controls = ({ duration, playing, position, mediaApi, liftState, pause, play }) => {
   const handlePlayButtonClick = () => {
-    playing ? mediaApi.pause() : mediaApi.play();
-    liftState({playing: !playing});
+    playing ? pause() : play();
   };
 
   const handleSeek = (time) => {
     mediaApi.seek(time);
     liftState({ position: time });
-  }
+  };
 
   return (
     <div>
-      <PlayButton onClick={handlePlayButtonClick} playing={playing}/>
-      <ProgressBar
-        handleSeek={handleSeek}
-        duration={duration}
-        position={position}
-      />
-      <TimeDisplay
-        currentTime={position}
-        totalTime={duration}
-      />
+      <PlayButton onClick={handlePlayButtonClick} playing={playing} />
+      <ProgressBar handleSeek={handleSeek} duration={duration} position={position} />
+      <TimeDisplay currentTime={position} totalTime={duration} />
     </div>
   );
 };
 
-export default Controls;
+const mapStateToProps = (state) => ({
+  playing: state.media.playing,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  play: () => dispatch(play()),
+  pause: () => dispatch(pause()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Controls);
